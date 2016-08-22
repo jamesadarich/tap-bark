4f5a607741460bf40c7594ad50bdb5b47a19ca06
James Monger <jameskmonger@hotmail.co.uk>
2016-08-22 22:55:29 +0100

    Inject stream and event emitter

--- src/tap-bark.ts
+++ src/tap-bark.ts
@@ -3,9 +3,11 @@ import { IStream, Stream } from "./_stream";
 export class TapBark {
 
     private _stream: IStream;
+    private _parser: NodeJS.EventEmitter;
 
-    constructor (stream: IStream) {
+    constructor (stream: IStream, parser: NodeJS.EventEmitter) {
         this._stream = stream;
+        this._parser = parser;
     }
 
 }
