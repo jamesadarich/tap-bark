0d3d28d831679750d8d8978ff5adab3cebc2554e
James Monger <jameskmonger@hotmail.co.uk>
2016-08-22 22:48:10 +0100

    Stream class

--- src/stream/stream.ts
+++ src/stream/stream.ts
@@ -1,5 +1,9 @@
 export class Stream {
 
-    private _stream = process.stdout;
+    private _stream: NodeJS.WritableStream = process.stdout;
+
+    public writeLine(message: string): void {
+        this._stream.write(message + "\n");
+    }
 
 }
