86c0923bc778b8e90f7f7559506923d2fb6387db
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-08-22 18:13:19 +0100

    Index POC

---
+++ index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+let stream = process.stdout;
+
+let clearCurrentLine = () => {
+    stream.clearLine();
+    stream.cursorTo(0);
+};
+
+let writeTestFixture = (name) => {
+    stream.write(`# [${name}]`);
+};
+
+let writeTestCase = (name) => {
+    stream.write(name);
+};
+
+let replaceTestFixture = (name) => {
+    stream.moveCursor(0, -2);
+    clearCurrentLine();
+    writeTestFixture(name);
+    stream.moveCursor(0, 2);
+    stream.cursorTo(0);
+};
+
+let replaceTestCase = (name) => {
+    stream.moveCursor(0, -1);
+    clearCurrentLine();
+    writeTestCase(name);
+    stream.moveCursor(0, 1);
+    stream.cursorTo(0);
+};
+
+writeTestFixture("SomeTestFixture");
+stream.write("\n");
+writeTestCase("testCaseOne");
+stream.write("\n");
+
+setTimeout(() => {
+    replaceTestCase("testCaseTwo");
+
+    setTimeout(() => {
+        replaceTestFixture("AnotherTestFixture");
+        replaceTestCase("firstTestCaseHere");
+
+        setTimeout(() => {
+            replaceTestCase("ThisIsTheLast");
+        }, 600);
+    }, 1200);
+}, 500);
