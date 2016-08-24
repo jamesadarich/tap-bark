0456efbd93522dab9072e755ccb498f2adc84413
James Monger <jameskmonger@hotmail.co.uk>
2016-08-24 22:20:58 +0100

    Implement getResultMessage

--- src/output-provider/output-provider.ts
+++ src/output-provider/output-provider.ts
@@ -1,8 +1,20 @@
 import { IOutputProvider } from "./output-provider.i";
 import { ResultType } from "../result-type";
+const chalk = require("chalk");
 
 export class OutputProvider implements IOutputProvider {
     public getResultMessage(type: ResultType, typeCount: number, totalCount: number): string {
+        switch (type) {
+            case ResultType.PASS:
+                return chalk.green(`Pass: ${typeCount}/${totalCount}`);
+
+            case ResultType.FAIL:
+                return chalk.red(`Fail: ${typeCount}/${totalCount}`);
+
+            case ResultType.IGNORE:
+                return chalk.yellow(`Ignore: ${typeCount}/${totalCount}`);
+        }
+
         return undefined;
     }
 
