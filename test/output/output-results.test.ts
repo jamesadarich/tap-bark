eaa0c3d8261c5e13021f9391cd32b15b205db073
James Richford <jamesrichford@googlemail.com>
2016-10-13 18:56:13 +0100

    fix bizarrely failing test

--- test/output/output-results.test.ts
+++ test/output/output-results.test.ts
@@ -36,8 +36,8 @@ export class OutputResultsTests {
             .build();
 
         output.outputResults(results);
-        
-        Expect(stream.writeLine).not.toHaveBeenCalledWith(`p ${passes} ${passes}`);
+
+        Expect(stream.writeLine).toHaveBeenCalledWith(`p ${passes} ${passes}`);
     }
 
     @TestCase(5)
