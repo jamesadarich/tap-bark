760b74e9548b9707c7e6da69316e3ed6ff9d6848
jamesrichford <jamesrichford@googlemail.com>
2016-11-24 12:14:41 +0000

    index tests

--- test/unit-tests/index.test.ts
+++ test/unit-tests/index.test.ts
@@ -2,9 +2,57 @@ import { Test, Expect, SpyOn } from "alsatian";
 import { TapBark } from "../../src/tap-bark";
 
 export default class IndexTests {
-    public tapBarkPipedToStdIn() {
-        SpyOn(process.stdin, "pipe");
 
-        Expect(process.stdin.pipe).toHaveBeenCalledWith(TapBark.create().getPipeable());
+    @Test("process.stdin stream piped to Tap Bark")
+    public stdInPipedToTapBark() {
+
+        const chainedPipe = { pipe: () => {} };
+
+        SpyOn(process.stdin, "pipe").andReturn(chainedPipe);
+
+        const fakeTapBarkPipeable = {
+            on: () => {},
+            once: () => {},
+            emit: () => {}
+        };
+
+        const fakeTapBark = { getPipeable: () => fakeTapBarkPipeable };
+
+        SpyOn(TapBark, "create").andReturn(fakeTapBark);
+
+        // clear cache for index
+        delete require.cache[require.resolve("../../index")];
+
+        // call the index
+        require("../../index");
+
+        Expect(process.stdin.pipe).toHaveBeenCalledWith(fakeTapBarkPipeable);
+    }
+
+    @Test("Tap Bark stream piped to process.stdout")
+    public tapBarkPipedToStdOut() {
+
+        const chainedPipe = { pipe: () => {} };
+        SpyOn(chainedPipe, "pipe");
+
+        SpyOn(process.stdin, "pipe").andReturn(chainedPipe);
+
+        const fakeTapBarkPipeable = {
+            on: () => {},
+            once: () => {},
+            emit: () => {}
+        };
+
+        const fakeTapBark = { getPipeable: () => fakeTapBarkPipeable };
+
+        SpyOn(TapBark, "create").andReturn(fakeTapBark);
+
+        // clear cache for index
+        delete require.cache[require.resolve("../../index")];
+
+        // call the index
+        require("../../index");
+
+        Expect(chainedPipe.pipe).toHaveBeenCalledWith(process.stdout);
     }
 }
