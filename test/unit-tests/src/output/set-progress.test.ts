6a77dfd5b2b366a7b68ca2a8d30c9f85302e7967
James Richford <jamesrichford@googlemail.com>
2016-11-25 08:00:59 +0000

    progress tests

--- test/unit-tests/src/output/set-progress.test.ts
+++ test/unit-tests/src/output/set-progress.test.ts
@@ -19,11 +19,68 @@ export default class SetProgressTests {
         Expect(stream.moveCursor).toHaveBeenCalledWith(0, -1);
     }
 
-    // stream to clear line
+    @Test()
+    public streamCursorClearsLine() {
+        const stream = new StreamBuilder().build();
+        const outputProvider = new OutputProviderBuilder().build();
 
-    // progress bar set (stream.write)
+        const output = new Output(stream, outputProvider);
 
-    // stream move cursor to 0, 1
+        SpyOn(stream, "clearLine");
 
-    // stream cursor to 0, undefined
+        output.setProgress(100, 100);
+
+        Expect(stream.clearLine).toHaveBeenCalled();
+    }
+
+    @TestCase(0, 1, "|                    |")
+    @TestCase(0, 10, "|                    |")
+    @TestCase(0, 100, "|                    |")
+    @TestCase(1, 1, "|====================|")
+    @TestCase(1, 10, "|==                  |")
+    @TestCase(1, 100, "|                    |")
+    @TestCase(4, 10, "|========            |")
+    @TestCase(42, 100, "|========            |")
+    @TestCase(10, 10, "|====================|")
+    @TestCase(100, 100, "|====================|")
+    public streamIsWrittenWithCorrectProgressBar(currentProgress: number, total: number, expectedProgressBar: string) {
+        const stream = new StreamBuilder().build();
+        const outputProvider = new OutputProviderBuilder().build();
+
+        const output = new Output(stream, outputProvider);
+
+        SpyOn(stream, "write");
+
+        output.setProgress(currentProgress, total);
+
+        Expect(stream.write).toHaveBeenCalledWith(expectedProgressBar);
+    }
+
+    @Test()
+    public streamCursorMovesToZeroOne() {
+        const stream = new StreamBuilder().build();
+        const outputProvider = new OutputProviderBuilder().build();
+
+        const output = new Output(stream, outputProvider);
+
+        SpyOn(stream, "moveCursor");
+
+        output.setProgress(100, 100);
+
+        Expect(stream.moveCursor).toHaveBeenCalledWith(0, 1);
+    }
+
+    @Test()
+    public streamCursorFinishesByPositioningCursorAtZeroUndefined() {
+        const stream = new StreamBuilder().build();
+        const outputProvider = new OutputProviderBuilder().build();
+
+        const output = new Output(stream, outputProvider);
+
+        SpyOn(stream, "cursorTo");
+
+        output.setProgress(100, 100);
+
+        Expect(stream.cursorTo).toHaveBeenCalledWith(0, undefined);
+    }
 }
