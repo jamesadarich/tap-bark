804ff452f844e2d732cf76adcae4cd53eb7d07b5
James Richford <jamesrichford@googlemail.com>
2016-11-25 07:51:14 +0000

    check cursor moved to start off with

--- test/output/set-progress.test.ts
+++ test/output/set-progress.test.ts
@@ -1,8 +1,23 @@
-import { Test, Expect } from "alsatian";
+import { Test, TestCase, Expect } from "alsatian";
+import { StreamBuilder } from "../../../_builders/stream-builder";
+import { OutputProviderBuilder } from "../../../_builders/output-provider-builder";
+import { Output } from "../../src/output/output";
 
 export default class SetProgressTests {
 
-    // stream move cursor to 0, -1
+    @Test()
+    public streamCursorStartsByMovingToZeroNegativeOne() {
+        const stream = new StreamBuilder.build();
+        const outputProvider = new OutputProviderBuilder.build();
+
+        const output = new Output(stream, outputProvider);
+
+        SpyOn(stream, "moveCursor");
+
+        output.setProgress(100, 100);
+
+        Expect(stream.moveCursor).toHaveBeenCalledWith(0, -1);
+    }
 
     // stream to clear line
 
