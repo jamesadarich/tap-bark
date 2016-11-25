c4b0e882958171434e103e8fa566f9a7238bf653
James Richford <jamesrichford@googlemail.com>
2016-11-25 07:45:31 +0000

    plan out set progress tests

---
+++ test/output/set-progress.test.ts
@@ -0,0 +1,14 @@
+import { Test, Expect } from "alsatian";
+
+export default class SetProgressTests {
+
+    // stream move cursor to 0, -1
+
+    // stream to clear line
+
+    // progress bar set (stream.write)
+
+    // stream move cursor to 0, 1
+
+    // stream cursor to 0, undefined
+}
