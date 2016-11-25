ac1701bc5c002ee9460f3fb09021111b7e40411b
James Richford <jamesrichford@googlemail.com>
2016-11-25 07:53:47 +0000

    update references

--- test/unit-tests/src/output/get-stream.test.ts
+++ test/unit-tests/src/output/get-stream.test.ts
@@ -1,7 +1,7 @@
 import { TestCase, Expect } from "alsatian";
-import { Output } from "../../src/output/output";
-import { IStream } from "../../src/stream/stream.i";
-import { IOutputProvider } from "../../src/output-provider/output-provider.i";
+import { Output } from "../../../../src/output/output";
+import { IStream } from "../../../../src/stream/stream.i";
+import { IOutputProvider } from "../../../../src/output-provider/output-provider.i";
 
 export default class GetStreamTests {
 
--- test/unit-tests/src/output/set-progress.test.ts
+++ test/unit-tests/src/output/set-progress.test.ts
@@ -1,14 +1,14 @@
-import { Test, TestCase, Expect } from "alsatian";
+import { Test, TestCase, Expect, SpyOn } from "alsatian";
 import { StreamBuilder } from "../../../_builders/stream-builder";
 import { OutputProviderBuilder } from "../../../_builders/output-provider-builder";
-import { Output } from "../../src/output/output";
+import { Output } from "../../../../src/output/output";
 
 export default class SetProgressTests {
 
     @Test()
     public streamCursorStartsByMovingToZeroNegativeOne() {
-        const stream = new StreamBuilder.build();
-        const outputProvider = new OutputProviderBuilder.build();
+        const stream = new StreamBuilder().build();
+        const outputProvider = new OutputProviderBuilder().build();
 
         const output = new Output(stream, outputProvider);
 
